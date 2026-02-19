8b23edcb7bb96b88bf372add6a615c0b557ab4f0
puneethreddytapasi <tpuneeth123@gmail.com>
2026-02-19 10:53:28 +0530

    Added UseCase3 Palindrome Checker with user input

--- src/UseCase3PalindromeCheckerApp.java
+++ src/UseCase3PalindromeCheckerApp.java
@@ -8,18 +8,12 @@ public class UseCase3PalindromeCheckerApp {
 
         System.out.print("Enter a string: ");
         String original = scanner.nextLine();
-
-        // Convert to lowercase to make it case-insensitive
         String input = original.toLowerCase();
 
         String reversed = "";
-
-        // Reverse string using for loop
         for (int i = input.length() - 1; i >= 0; i--) {
             reversed = reversed + input.charAt(i);
         }
-
-        // Compare using equals()
         if (input.equals(reversed)) {
             System.out.println("The given string is a Palindrome.");
         } else {
