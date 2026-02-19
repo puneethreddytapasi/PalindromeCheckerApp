6980972efadd24e1bed4d2d79de73046ef00fa1e
puneethreddytapasi <tpuneeth123@gmail.com>
2026-02-19 11:00:41 +0530

    Added UseCase4 Palindrome Checker APP

---
+++ src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class UseCase4PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        input = input.toLowerCase();
+        char[] characters = input.toCharArray();
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+        scanner.close();
+    }
+}
